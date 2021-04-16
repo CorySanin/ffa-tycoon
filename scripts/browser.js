e56ad6a4445dde030056ed6b35470e9913ca538a
Cory Sanin <corysanin@outlook.com>
2021-04-16 04:05:01 -0500

    Pagination, Bulma CSS framework

--- scripts/browser.js
+++ scripts/browser.js
@@ -21,8 +21,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
         });
     }
-    function getUrl() {
-        return `/archive/${state.page}?${new URLSearchParams({ orderby: state.orderby, order: state.order? 'ASC':'DESC' }).toString()}`;
+    function getUrl(s = state) {
+        return `/archive/${s.page}?${new URLSearchParams({ orderby: s.orderby, order: s.order ? 'ASC' : 'DESC' }).toString()}`;
     }
     function getParks(page = 1, orderby = 'date', order = false) {
         fetch(`/api/parks/${page}?${new URLSearchParams({ orderby, order }).toString()}`)
@@ -48,7 +48,7 @@ document.addEventListener("DOMContentLoaded", function () {
             td.appendChild(a);
             tr.appendChild(td);
             td = document.createElement('td');
-            td.appendChild(document.createTextNode((new Date(park.date)).toLocaleDateString() + ` (${park.date})`));
+            td.appendChild(document.createTextNode((new Date(park.date)).toLocaleDateString()));
             tr.appendChild(td);
             td = document.createElement('td');
             td.appendChild(document.createTextNode(park.groupname));
@@ -62,12 +62,70 @@ document.addEventListener("DOMContentLoaded", function () {
             tableBody.appendChild(tr);
         });
     }
+    function createPaginationLink(pagenum, text, classlist = [], current = false) {
+        let child;
+        if (text == null && pagenum != null) {
+            text = pagenum;
+        }
+        if (pagenum != null) {
+            child = document.createElement('a');
+            child.appendChild(document.createTextNode(text));
+            child.href = getUrl({
+                page: pagenum,
+                orderby: state.orderby,
+                order: state.order
+            });
+            if (current) {
+                child.classList.add('is-current');
+            }
+        }
+        else {
+            child = document.createElement('span');
+            child.appendChild(document.createTextNode(text));
+
+        }
+        child.classList.add(classlist);
+        return child;
+    }
+    function createPaginationNode(pagenum, text, classlist = [], current = false) {
+        let li;
+        li = document.createElement('li');
+        li.appendChild(createPaginationLink(pagenum, text, classlist, current));
+        return li;
+    }
     function updatePagination(pages = 1, currentPage = 1) {
         while (pagination.firstChild) {
             pagination.removeChild(pagination.lastChild);
         }
         if (pages > 1) {
+            const first = 1;
+            const minpage = Math.max(1, currentPage - 4);
+            const maxpage = Math.min(pages, currentPage + 4);
+            let a, ul = document.createElement('ul');
+            ul.classList.add('pagination-list');
 
+            if (currentPage > first) {
+                pagination.appendChild(createPaginationLink(state.page - 1, 'Previous', 'pagination-previous'));
+                if (minpage > first) {
+                    ul.appendChild(createPaginationNode(first, null, 'pagination-link'));
+                    if (minpage > first + 1) {
+                        ul.appendChild(createPaginationNode(null, '\u2026', 'pagination-ellipsis'));
+                    }
+                }
+            }
+            for (let i = minpage; i <= maxpage; i++) {
+                ul.appendChild(createPaginationNode(i, null, 'pagination-link', i === currentPage));
+            }
+            if (currentPage < pages) {
+                if (maxpage < pages) {
+                    if (maxpage < pages - 1) {
+                        ul.appendChild(createPaginationNode(null, '\u2026', 'pagination-ellipsis'));
+                    }
+                    ul.appendChild(createPaginationNode(pages, null, 'pagination-link'));
+                }
+                pagination.appendChild(createPaginationLink(state.page + 1, 'Next', 'pagination-next'));
+            }
+            pagination.appendChild(ul);
         }
     }
     let update = () => {
